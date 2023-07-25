78580daa9ad557692c54e443317cd4761ec95fad
Yuri <gr033@naver.com>
2023-07-25 22:09:48 +0900

    메일링 다시 합쳤어요. 죄송함다...

--- src/main/java/com/example/demo/controller/MailController.java
+++ src/main/java/com/example/demo/controller/MailController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
@@ -13,6 +15,39 @@ import lombok.Setter;
 public class MailController {
 	@Autowired
 	private MailSender mailSender;
+	
+	// 4자리 난수를 발행하여 메일로 전송 해 봅니다.
+		@GetMapping("/emailCheck2")
+		public String emailCheck(String email) {
+
+			SimpleMailMessage mailMessage = 
+					new SimpleMailMessage();
+			
+			Random r = new Random();
+			int a = r.nextInt(10); //0~9
+			int b = r.nextInt(10); //0~9
+			int c = r.nextInt(10); //0~9
+			int d = r.nextInt(10); //0~9
+			
+			String data = "<h1>해당 인증번호를 입력해 주세요!</h1>";
+			data += "<hr>"
+					+ "<h2>"
+			 +a+""+b+""+c+""+d+"</h2>";
+			
+			
+			mailMessage.setFrom("qlqlql8448@gmail.com");
+			mailMessage.setTo(email);
+			mailMessage.setSubject("Ketchby 이메일 인증");
+			mailMessage.setText(data);
+			
+			try {
+				mailSender.send(mailMessage);
+			}catch (Exception e) {
+				System.out.println("예외발생:"+e.getMessage());
+			}
+			
+			return "OK";
+		}
 	
 	@GetMapping("/admin/")
 	public String mail() {
@@ -30,4 +65,8 @@ public class MailController {
 		}
 		return "OK";
 	}
+	
+	
+	
+	
 }
