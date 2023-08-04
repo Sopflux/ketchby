7ee062f3bb901a3562de9bd022ad7dafbc8fecc3
hyunseo3 <svtboo0526@gmail.com>
2023-08-04 11:15:04 +0900

    Q&A Main 완료 (헤더 푸터 제외)

--- src/main/java/com/example/demo/controller/QandAController.java
+++ src/main/java/com/example/demo/controller/QandAController.java
@@ -1,26 +1,39 @@
 package com.example.demo.controller;
 
-import java.util.Map;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.example.demo.ApiBigQueryAuthentication;
-import com.example.demo.service.QandAService;
+import com.example.demo.dao.QandADAO;
+import com.example.demo.entity.QandA;
 
+import autovalue.shaded.com.google.common.base.Optional;
 import lombok.Setter;
 
 @Controller
 @Setter
 public class QandAController {
 	@Autowired
-	private QandAService qs;
+	private QandADAO dao;
+
 	
 	@GetMapping("/qanda/qandamain")
-	public void list(Model model, int qno) {
-		model.addAttribute("list",qs.findByQno(qno));
+	public void list() {}
+	
+	@GetMapping("/qanda/qandamain/ajax")
+	@ResponseBody
+	public List<QandA> findList(int qno) {
+	    List<QandA> list = null;
+	    if (qno == 0) {
+	        list = dao.findAll();
+	    } else {
+	        list = dao.findByQno(qno);
+	    }
+	    return list;
 	}
 }
