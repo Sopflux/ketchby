eadd57951ccc0eb40e41d4c5bd2c28b3257d2e76
Sop <sopjeong@gmail.com>
2023-08-12 23:47:42 +0900

    mainPage css 수정중 배너, 카테고리 완료

--- src/main/java/com/example/demo/entity/MainPage.java
+++ src/main/java/com/example/demo/entity/MainPage.java
@@ -42,5 +42,145 @@ public class MainPage {
 	private String cbimg;
 	private String cbclsdate;
 	private int slocno;
+	public int getClno() {
+		return clno;
+	}
+	public void setClno(int clno) {
+		this.clno = clno;
+	}
+	public int getScano() {
+		return scano;
+	}
+	public void setScano(int scano) {
+		this.scano = scano;
+	}
+	public String getCltitle() {
+		return cltitle;
+	}
+	public void setCltitle(String cltitle) {
+		this.cltitle = cltitle;
+	}
+	public String getCladdr() {
+		return claddr;
+	}
+	public void setCladdr(String claddr) {
+		this.claddr = claddr;
+	}
+	public String getClimg() {
+		return climg;
+	}
+	public void setClimg(String climg) {
+		this.climg = climg;
+	}
+	public String getCllevel() {
+		return cllevel;
+	}
+	public void setCllevel(String cllevel) {
+		this.cllevel = cllevel;
+	}
+	public int getClprice() {
+		return clprice;
+	}
+	public void setClprice(int clprice) {
+		this.clprice = clprice;
+	}
+	public String getScaname() {
+		return scaname;
+	}
+	public void setScaname(String scaname) {
+		this.scaname = scaname;
+	}
+	public String getBcaname() {
+		return bcaname;
+	}
+	public void setBcaname(String bcaname) {
+		this.bcaname = bcaname;
+	}
+	public String getAid() {
+		return aid;
+	}
+	public void setAid(String aid) {
+		this.aid = aid;
+	}
+	public String getLevel_() {
+		return level_;
+	}
+	public void setLevel_(String level_) {
+		this.level_ = level_;
+	}
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getBtitle() {
+		return btitle;
+	}
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+	public String getBcontent() {
+		return bcontent;
+	}
+	public void setBcontent(String bcontent) {
+		this.bcontent = bcontent;
+	}
+	public String getBdate() {
+		return bdate;
+	}
+	public void setBdate(String bdate) {
+		this.bdate = bdate;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public int getCbno() {
+		return cbno;
+	}
+	public void setCbno(int cbno) {
+		this.cbno = cbno;
+	}
+	public String getCbtitle() {
+		return cbtitle;
+	}
+	public void setCbtitle(String cbtitle) {
+		this.cbtitle = cbtitle;
+	}
+	public int getCbpeople() {
+		return cbpeople;
+	}
+	public void setCbpeople(int cbpeople) {
+		this.cbpeople = cbpeople;
+	}
+	public String getCbimg() {
+		return cbimg;
+	}
+	public void setCbimg(String cbimg) {
+		this.cbimg = cbimg;
+	}
+	public String getCbclsdate() {
+		return cbclsdate;
+	}
+	public void setCbclsdate(String cbclsdate) {
+		this.cbclsdate = cbclsdate;
+	}
+	public int getSlocno() {
+		return slocno;
+	}
+	public void setSlocno(int slocno) {
+		this.slocno = slocno;
+	}
+	
+	
 	
 }
