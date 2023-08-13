84402d27e8ba5b081eb366d0b1c04627f4b8e2e5
hyunseo3 <94173709+hyunseo3@users.noreply.github.com>
2023-08-13 16:15:52 +0900

    QnainsertVO
    QnainsertVO

---
+++ src/main/java/com/example/demo/entity/QnainsertVO.java
@@ -0,0 +1,16 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+
+@Data
+public class QnainsertVO {
+	private int qino;
+	private int qno;
+	private String qiname;
+	private String qiemail;
+	private String qititle;
+	private String qicontent;
+	private String qidate;
+	private String qicondition;
+}
